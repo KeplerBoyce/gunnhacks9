bd97721ea4046c6c8f306e6b859d98712b14e5fc
CrolineCrois <maoninii@gmail.com>
2023-01-21 17:58:13 -0800

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container pt-4">
-        <h1 className="text-center text-5xl font-bold text-red-500">
-          Gunnhacks 9
-        </h1>
+      <main className="flex flex-col items-center container pt-1 py-1">
+        <div className="bg-gray-100">
+          <h1 className="text-center text-6xl font-bold text-red-500 mt-10 ">
+            Piano Chord Practice
+          </h1>
+          <h2 className="text-center mb-10 text-2xl text-red-400 mt-5">
+            GunnHacks 9.0
+          </h2>
+        </div>
+        <div className="flex items-center mt-32">
+          <a href="https://www.youtube.com" className="button w-min whitespace-nowrap text-white bg-red-500 text-5xl border-0 py-4 px-8 rounded-lg">Go To Exercise</a>
+        </div>
       </main>
     </div>
+
+    
   )
 }
