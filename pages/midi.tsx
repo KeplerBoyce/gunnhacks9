0c363bafb9e8cd6778d599672b9c04e314965131
qpwoeirut <stanleyzhong8@gmail.com>
2023-01-21 18:09:41 -0800

    Add content presentation to proof-of-concept

--- pages/midi.tsx
+++ pages/midi.tsx
@@ -1,52 +1,72 @@
 import Head from "next/head";
-import { useEffect } from "react";
 import {WebMidi} from "webmidi";
+import {useState} from "react";
 
+enum LoadingState {
+  WAITING, NO_DEVICE, DEVICE_FOUND
+}
 
+function MidiContent(props: { devices: JSX.Element[], notes: JSX.Element[] }) {
+  return <div>
+    <h2 className="text-center text-3xl font-bold">Devices</h2>
+    <ul>{props.devices}</ul>
+
+    <h2 className="text-center text-3xl font-bold">Notes</h2>
+    <ul>{props.notes}</ul>
+  </div>
+}
+
+function PageContent(props: { loadState: LoadingState, devices: JSX.Element[], notes: JSX.Element[] }) {
+  switch (props.loadState) {
+    case LoadingState.WAITING:
+      return <p>Loading...</p>
+    case LoadingState.NO_DEVICE:
+      return <p>No device found</p>
+    case LoadingState.DEVICE_FOUND:
+      return <MidiContent devices={props.devices} notes={props.notes}/>;
+  }
+}
 
 export default function Midi() {
-  useEffect(doStuff,[]);
+  const [loadState, setLoadState] = useState(LoadingState.WAITING);
+  const [devices, setDevices] = useState<JSX.Element[]>([]);
+  const [notes, setNotes] = useState<JSX.Element[]>([]);
 
+  function onEnabled() {
+    // Display available MIDI input devices
+    if (WebMidi.inputs.length >= 1) {
+      setLoadState(LoadingState.DEVICE_FOUND);
+
+      setDevices(WebMidi.inputs.map((device, index) => <li key={index}>{index}: {device.name}</li>));
+
+      midi.channels[1].addListener("noteon", e => {
+        setNotes(prevState => [
+          ...prevState, <li key="a">{e.note.identifier + Math.floor(e.note.number / 12 - 1)}</li>
+        ]);
+      });
+    } else setLoadState(LoadingState.NO_DEVICE);
+  }
+
+  const midi = WebMidi.inputs[0];
+
+  WebMidi
+    .enable()
+    .then(onEnabled)
+    .catch(err => alert(err));
   return (
     <div>
       <Head>
         <title>MIDI PAGE</title>
-        <meta name="description" content="Funny Site" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Funny Site"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main className="container pt-4">
         <h1 className="text-center text-5xl font-bold text-red-500">
           MIDI
         </h1>
+        <PageContent loadState={loadState} devices={devices} notes={notes}/>
       </main>
     </div>
   )
 }
-
-const doStuff = () => {
-  WebMidi
-  .enable()
-  .then(onEnabled)
-  .catch(err => alert(err));
-
-  // Function triggered when WEBMIDI.js is ready
-  function onEnabled() {
-
-  // Display available MIDI input devices
-  if (WebMidi.inputs.length < 1) {
-    document.body.innerHTML+= "No device detected.";
-  } else {
-    WebMidi.inputs.forEach((device, index) => {
-      document.body.innerHTML+= `${index}: ${device.name} <br>`;
-    });
-  }
-
-  const midi = WebMidi.inputs[0];
-  
-  midi.channels[1].addListener("noteon", e => {
-    document.body.innerHTML+= `${e.note.identifier + Math.floor(e.note.number / 12 - 1)} <br>`;
-  });
-
-  }
-};
